c0e1ae09733f5593505ba865796e10d879107b48
Alhajras <kharabeash@gmail.com>
2018-02-18 21:42:39 +0100

    Adding the first spring configurations

---
+++ Jay3an/src/main/java/controllers/BootGradleApplication.java
@@ -0,0 +1,14 @@
+package controllers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class BootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleApplication.class, args);
+		 
+	}
+
+}
